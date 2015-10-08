e1a498a332263ce607449af87151725962b2bc3e
jan <git@sternprodukt.de>
2015-10-09 00:33:37 +0200

    refactor without breaking tests

--- addon/components/pikaday-input.js
+++ addon/components/pikaday-input.js
@@ -6,6 +6,7 @@ export default Ember.Component.extend({
   tagName: 'input',
   attributeBindings: ['readonly', 'disabled', 'placeholder', 'type'],
   type: 'text',
+  pikaday: null,
 
   // options
   value: null,
@@ -19,9 +20,15 @@ export default Ember.Component.extend({
   minDate: null,
   maxDate: null,
   useUTC: null,
-  i18n: null,
+  i18n: undefined,
 
-  setupPikaday: Ember.on('didInsertElement', function() {
+  valueMoment: Ember.computed('value', function() {
+    let val = this.get('value');
+    let format = this.get('format');
+    return moment(val, format);
+  }),
+
+  didInsertElement() {
     let that = this;
     let firstDay = this.get('firstDay');
 
@@ -31,18 +38,20 @@ export default Ember.Component.extend({
       onClose: Ember.run.bind(this, this.onPikadayClose),
       onSelect: Ember.run.bind(this, this.onPikadaySelect),
       onDraw: Ember.run.bind(this, this.onPikadayRedraw),
-      firstDay: firstDay ? parseInt(firstDay, 10) : 1,
+      firstDay: (firstDay != null) ? parseInt(firstDay, 10) : 1,
       format: this.get('format'),
       yearRange: that.determineYearRange(),
       minDate: this.get('minDate'),
       maxDate: this.get('maxDate'),
-      theme: this.get('theme'),
-      i18n: this.get('i18n')
+      theme: this.get('theme')
     };
 
-    var pikaday = new Pikaday(options);
+    if (this.get('i18n')) { options.i18n = this.get('i18n'); }
+
+    let pikaday = new Pikaday(options);
 
     this.set('pikaday', pikaday);
+
     this.setPikadayDate();
 
     this.addObserver('value', function() {
@@ -56,7 +65,7 @@ export default Ember.Component.extend({
     this.addObserver('maxDate', function() {
       this.setMaxDate();
     });
-  }),
+  },
 
   teardownPikaday: Ember.on('willDestroyElement', function() {
     this.get('pikaday').destroy();
@@ -89,7 +98,7 @@ export default Ember.Component.extend({
   onPikadayRedraw: Ember.K,
 
   userSelectedDate: function() {
-    var selectedDate = this.get('pikaday').getDate();
+    let selectedDate = this.get('pikaday').getDate();
 
     if (this.get('useUTC')) {
       selectedDate = moment.utc([selectedDate.getFullYear(), selectedDate.getMonth(), selectedDate.getDate()]).toDate();
@@ -99,11 +108,11 @@ export default Ember.Component.extend({
   },
 
   determineYearRange: function() {
-    var yearRange = this.get('yearRange');
+    let yearRange = this.get('yearRange');
 
     if (yearRange) {
       if (yearRange.indexOf(',') > -1) {
-        var yearArray = yearRange.split(',');
+        let yearArray = yearRange.split(',');
 
         if (yearArray[1] === 'currentYear') {
           yearArray[1] = new Date().getFullYear();
