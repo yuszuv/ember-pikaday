ed8c103e93084755c0e546d3f7c0fce5e780cb8b
jan <git@sternprodukt.de>
2015-10-08 23:17:07 +0200

    add pikaday theme by option

--- index.js
+++ index.js
@@ -7,8 +7,15 @@ module.exports = {
   included: function(app) {
     this._super.included(app);
 
+    var options = app.options['ember-pikaday'] || { theme: true };
+
+    if (options.theme) {
+      app.import(app.bowerDirectory + '/pikaday/css/pikaday.css');
+    }
+
+    // import javascript
     app.import(app.bowerDirectory + '/moment/moment.js');
     app.import(app.bowerDirectory + '/pikaday/pikaday.js');
-    app.import(app.bowerDirectory + '/pikaday/css/pikaday.css');
   }
+
 };
